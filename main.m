bf00cf940e540b7b2aa32d618338281124153565
Xinchao1995 <64560217+Xinchao1995@users.noreply.github.com>
2021-11-21 22:12:24 -0600

    Add files via upload

--- main.m
+++ main.m
@@ -23,11 +23,11 @@ t3=clock;
 %---------------------------------------------------------------------%
 % Part 3: Plot q, F, POD basis: (estimated time:0.3898mins)
 %---------------------------------------------------------------------%
-%Fig.10 (a)
+%Fig.9 (a)
 PlotState(c1_F_ROM_new,c1_StateData_displacement_ROM_new,c2_F_ROM_new,c2_StateData_displacement_ROM_new,c3_F_ROM_new,c3_StateData_displacement_ROM_new,c4_F_ROM_new,c4_StateData_displacement_ROM_new,c5_F_ROM_new,c5_StateData_displacement_ROM_new,c6_F_ROM_new,c6_StateData_displacement_ROM_new,c7_F_ROM_new,c7_StateData_displacement_ROM_new,c8_F_ROM_new,c8_StateData_displacement_ROM_new,c9_F_ROM_new,c9_StateData_displacement_ROM_new,c10_F_ROM_new,c10_StateData_displacement_ROM_new,c11_F_ROM_new,c11_StateData_displacement_ROM_new,c12_F_ROM_new,c12_StateData_displacement_ROM_new,c13_F_ROM_new,c13_StateData_displacement_ROM_new,c14_F_ROM_new,c14_StateData_displacement_ROM_new,c15_F_ROM_new,c15_StateData_displacement_ROM_new,c16_F_ROM_new,c16_StateData_displacement_ROM_new,c17_F_ROM_new,c17_StateData_displacement_ROM_new,c18_F_ROM_new,c18_StateData_displacement_ROM_new,c19_F_ROM_new,c19_StateData_displacement_ROM_new,c20_F_ROM_new,c20_StateData_displacement_ROM_new,c21_F_ROM_new,c21_StateData_displacement_ROM_new,c22_F_ROM_new,c22_StateData_displacement_ROM_new,c23_F_ROM_new,c23_StateData_displacement_ROM_new,c24_F_ROM_new,c24_StateData_displacement_ROM_new,c25_F_ROM_new,c25_StateData_displacement_ROM_new,c26_F_ROM_new,c26_StateData_displacement_ROM_new,c27_F_ROM_new,c27_StateData_displacement_ROM_new,c28_F_ROM_new,c28_StateData_displacement_ROM_new,c29_F_ROM_new,c29_StateData_displacement_ROM_new,c30_F_ROM_new,c30_StateData_displacement_ROM_new,c31_F_ROM_new,c31_StateData_displacement_ROM_new,c32_F_ROM_new,c32_StateData_displacement_ROM_new,c33_F_ROM_new,c33_StateData_displacement_ROM_new,c34_F_ROM_new,c34_StateData_displacement_ROM_new,c35_F_ROM_new,c35_StateData_displacement_ROM_new);
-%Fig.10 (b)
+%Fig.9 (b)
 PlotForce(c1_F_ROM_new_cum2,c2_F_ROM_new_cum2,c3_F_ROM_new_cum2,c4_F_ROM_new_cum2,c5_F_ROM_new_cum2,c6_F_ROM_new_cum2,c7_F_ROM_new_cum2,c8_F_ROM_new_cum2,c9_F_ROM_new_cum2,c10_F_ROM_new_cum2,c11_F_ROM_new_cum2,c12_F_ROM_new_cum2,c13_F_ROM_new_cum2,c14_F_ROM_new_cum2,c15_F_ROM_new_cum2,c16_F_ROM_new_cum2,c17_F_ROM_new_cum2,c18_F_ROM_new_cum2,c19_F_ROM_new_cum2,c20_F_ROM_new_cum2,c21_F_ROM_new_cum2,c22_F_ROM_new_cum2,c23_F_ROM_new_cum2,c24_F_ROM_new_cum2,c25_F_ROM_new_cum2,c26_F_ROM_new_cum2,c27_F_ROM_new_cum2,c28_F_ROM_new_cum2,c29_F_ROM_new_cum2,c30_F_ROM_new_cum2,c31_F_ROM_new_cum2,c32_F_ROM_new_cum2,c33_F_ROM_new_cum2,c34_F_ROM_new_cum2,c35_F_ROM_new_cum2);
-%Fig.9 (1)
+%Fig.8 (1)
 index_PODbasis = 1; PlotPODbasis(Uhat(:,index_PODbasis)'); %index_PODbasis can be set to be 1~30, as which POD basis you want to check
 t4=clock;
 %%
@@ -61,11 +61,16 @@ t8=clock;
 %---------------------------------------------------------------------%
 % Part 8: Visualization: (estimated time:1.552mins)
 %---------------------------------------------------------------------%
-PlotMSE(Uhat,full_predict_displacement_POD,c35_StateData_displacement);%Fig.17: line #35
-PlotNodalDeformation(4999,r,Uhat,full_predict_displacement_POD,c35_StateData_displacement,displacementFPCAbasis,covariance_FPCA_displacement);%Fig.11 the first subplot
-PlotDisplacementXpre(400,Uhat,full_predict_displacement_POD);%Fig.13 top
-PlotDisplacementXactual(370,c35_StateData_displacement);%Fig.13 middle
-PlotXstandarddeviation(Uhat,covariance_POD_displacement);%Fig.13 bottom
+%Fig.14: line #35
+PlotMSE(Uhat,full_predict_displacement_POD,c35_StateData_displacement);
+%Fig.10 the first subplot
+PlotNodalDeformation(4999,r,Uhat,full_predict_displacement_POD,c35_StateData_displacement,displacementFPCAbasis,covariance_FPCA_displacement);
+%Fig.11 top
+PlotDisplacementXpre(400,Uhat,full_predict_displacement_POD);
+%Fig.11 middle
+PlotDisplacementXactual(370,c35_StateData_displacement);
+%Fig.11 bottom
+PlotXstandarddeviation(Uhat,covariance_POD_displacement);
 t9=clock;
 
 
